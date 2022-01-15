7e73fe14f36729d8cf217b1176ec7530fdc93281
talentedasian <lucenaalmirjames123@gmail.com>
2022-01-15 12:36:14 +0800

    Init commit

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+
+void avg(int limit) { 
+    float sum;
+    for (int i = 0; i < limit; i++) {
+        sum += (float)i + 0.723;
+    }
+    const float avg = sum / (float)10;
+    printf("The average is %f ", avg);
+
+}
+
+int main(void) {
+    stdio_init_all();
+
+
+    sleep_ms(6000);
+    int denoms[] = {1000000, 2000000, 7000000, 11000000, 20000000};
+    
+    for (int i = 0; i < 5; i++) {
+        long start_time = (long)get_absolute_time();
+        avg(denoms[i]);
+        long end_time = (long)get_absolute_time();
+    
+        printf("Total take time in seconds for %d is: %ld\n", denoms[i], 
+                (end_time - start_time) / (1000000));
+    }
+    
+}
